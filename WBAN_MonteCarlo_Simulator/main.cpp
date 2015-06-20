8615def60853e9ad6b69b7d4778531e5aabfd62a
Yang Zhou <yangzhou@dhcp-a4-5e-60-b9-05-d1.ubcvisitor.wifi.ubc.ca>
2015-06-19 18:11:49 -0700

    Initial Commit

---
+++ WBAN_MonteCarlo_Simulator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WBAN_MonteCarlo_Simulator
+//
+//  Created by Yang Zhou on 2015-06-19.
+//  Copyright (c) 2015 Yang Zhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
